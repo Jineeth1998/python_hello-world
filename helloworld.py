598315278353901ccbb04513faa2b744721fe264
Jineeth1998 <93464200+Jineeth1998@users.noreply.github.com>
2026-03-16 22:07:01 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,3 @@
-import time
+
 
 print("hello this is my first python program")
-
-def factorial(n):
-    fact = 1
-    for i in range(1,n+1):
-        fact *= i
-    return fact
-
-print("The factorial of 5 is", factorial(5))
-
-while True:
-    time.sleep(10)
