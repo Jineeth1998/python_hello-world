7a75ebf650ca831b740f7277009efd4134422967
Jineeth1998 <93464200+Jineeth1998@users.noreply.github.com>
2026-03-16 22:13:30 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hlo world"
+
+app.run(host="0.0.0.0", port=5000)
 
 print("hello this is my first python program")
