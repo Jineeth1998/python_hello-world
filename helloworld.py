3917559fdc9128a63cc659a520de25177b6e6b1d
Jineeth1998 <93464200+Jineeth1998@users.noreply.github.com>
2026-03-16 22:04:36 +0530

    add looping

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,14 @@
+import time
+
 print("hello this is my first python program")
 
-factorial = 1
-num = 5
-for i in range(1, num + 1):
-    factorial = factorial * i
-print("The factorial of", num, "is", factorial)
+def factorial(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact *= i
+    return fact
+
+print("The factorial of 5 is", factorial(5))
+
+while True:
+    time.sleep(10)
